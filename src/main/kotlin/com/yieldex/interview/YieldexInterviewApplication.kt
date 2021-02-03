a9e211eee5b2758965d283016d68cbb053166e90
aneshp92 <aneshp92@gmail.com>
2021-02-03 14:24:24 -0600

    first commit

---
+++ src/main/kotlin/com/yieldex/interview/YieldexInterviewApplication.kt
@@ -0,0 +1,11 @@
+package com.yieldex.interview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YieldexInterviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<YieldexInterviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/yieldex/interview/YieldexInterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yieldex.interview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YieldexInterviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
